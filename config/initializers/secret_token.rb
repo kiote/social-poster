9b3d2dd009a3f1b85834e6c6f469d3fba1549cb6
savva.voloshin <savva.voloshin@gmail.com>
2013-06-17 10:43:38 +0000

    skel of rails4 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SocialPoster::Application.config.secret_key_base = '3f69873f82deb12b0b01e1d66153947d8e1f06a32862d9c41862ec3df72c81c32ddf7f0bdb90a7df9406cd12414d0b91900bc17397c81eb393c4824a45b3dd08'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SocialPoster::Application.config.session_store :cookie_store, key: '_social_poster_session'
