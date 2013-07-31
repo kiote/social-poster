963f0892247267610f545a305d45e084e816341b
vcabba <savva.voloshin@gmail.com>
2013-07-31 08:14:48 +0000

    +primary behaviour descriptions

---
+++ features/primary_behaviour_tests.feature
@@ -0,0 +1,66 @@
+
+Feature: testing users behaviour
+
+  Scenario: main page ; sign in
+    Given user is on the main page
+    When user click sign in
+    Then user view sign_in page
+  
+  Scenario: main page ; sign up
+    Given user is on the main page
+    When user click sign up
+    Then user view sign_up page, whooa!
+  
+  Scenario: signing in; okay
+    Given user is on the sign in page
+    When fields Email, Password correctly filled
+    And button Sign In has been pressed
+    Then user can observe users#show page
+  
+  Scenario: signing in; kayo
+    Given user is on the sign in page
+    When fields Email, Password incorrectly filled
+    And button Sign In has been pressed
+    Then user can observe sign_in page
+    And error message
+  
+  Scenario: signing up; okay
+    Given user is on the sign up page
+    When 4 fields correctly filled
+    And button Create Account has been pressed
+    Then there new user appears
+    And his page with show action can be observed
+  
+  Scenario: signing up; okayn't
+    Given user is on the sign up page
+    When any of field is filled incorrectly
+    And button Create Account has been pressed
+    Then user can observe sign_up page again
+    And a message containing error description
+  
+  Scenario: Home page; signed in
+    Given user is on the Home page
+    And signed in
+    Then page contains form_for @message
+    
+  Scenario: Home page; unsigned
+    Given user is on the Home page
+    And unsigned
+    Then page contains things for welcome
+  
+  Scenario: Signed in user; view profile
+    Given user is on the Home page
+    And signed in
+    And clicked "view profile"
+    Then it just users#show/id action
+  
+  Scenario: Signed in user; view authorisations
+    Given user is on the Home page
+    And signed in
+    And clicked "view providers"
+    Then it just some action for TODO:
+  
+  Scenario: test for account' settings TODO:
+    Given TODO: that is given
+    Then TODO: ths is to do
+  
---
+++ features/step_definitions/step_primary_behaviour_tests.rb
@@ -0,0 +1,121 @@
+
+Given(/^user is on the main page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^user click sign in$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^user view sign_in page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^user click sign up$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^user view sign_up page, whooa!$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^user is on the sign in page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^fields Email, Password correctly filled$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^button Sign In has been pressed$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^user can observe users\#show page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^fields Email, Password incorrectly filled$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^user can observe sign_in page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^error message$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^user is on the sign up page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^(\d+) fields correctly filled$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^button Create Account has been pressed$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^there new user appears$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^his page with show action can be observed$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^any of field is filled incorrectly$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^user can observe sign_up page again$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^a message containing error description$/) do
+  pending # express the regexp above with the code you wish you had
+end
+Given(/^user is on the Home page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^signed in$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^page contains form_for @message$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^unsigned$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^page contains things for welcome$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^clicked "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^it just users\#show\/id action$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^it just some action for TODO:$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^TODO: that is given$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^TODO: ths is to do$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+
