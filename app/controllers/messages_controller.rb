a493a718aea1e39e961ac2050df21f58607cc3a5
vcabba <savva.voloshin@gmail.com>
2013-08-08 13:16:08 +0000

    testing sending messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,16 +10,29 @@ class MessagesController < ApplicationController
     
     Rails.logger.debug "> %s" % params.to_yaml
   
-    @message = current_user.messages.build sent_at: Time.new
+    @message = current_user.messages.build
     @message.build_texts(params)
     
-    message_sender = MessageSender.new(current_user)
-    message_sender.send_message(@message)
+    ms_fb = MessageSenderFacebook.new(current_user)
+    ms_ln = MessageSenderLinkedin.new(current_user)
+    ms_tu = MessageSenderTumblr.new(current_user)
+    ms_tw = MessageSenderTwitter.new(current_user)
+    ms_vk = MessageSenderVkontakte.new(current_user)
     
-    #~ sent_statuses = send_message(@message, current_user)
+    sent_statuses = Array.new
+    
+    sent_statuses << ms_fb.send_message(@message)
+    sent_statuses << ms_ln.send_message(@message)
+    sent_statuses << ms_tu.send_message(@message)
+    sent_statuses << ms_tw.send_message(@message)
+    sent_statuses << ms_vk.send_message(@message)
+    
+    sent_statuses = sent_statuses.join('; ')
+    
+    Rails.logger.debug "> sent statuses: %s" % sent_statuses
     
     if @message.save
-      flash[:succcess] = "message saved and sent with statuses: %s" % sent_statuses
+      flash[:success] = "message saved and sent with statuses: %s" % sent_statuses
     else
       flash[:fail] = "message not saved"
     end
